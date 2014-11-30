1968d6a8a7dc4eaba5d59229460df37cc64e3d45
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-30 02:48:25 -0500

    (feat) Clean up PATH and change LIBRARY_PATH.

--- runcoms/zshmodules/vars.zsh
+++ runcoms/zshmodules/vars.zsh
@@ -1,6 +1,7 @@
 # Env Vars
-export PATH=/usr/local/bin:/usr/local/opt/ccache/libexec:/Users/Jonathan/Library/Haskell/bin:$PATH:/Users/Jonathan/.cabal/bin:/Users/Jonathan/.rvm/gems/ruby-2.0.0-p247/bin:/Users/Jonathan/.rvm/gems/ruby-2.0.0-p247@global/bin:/Users/Jonathan/.rvm/rubies/ruby-2.0.0-p247/bin:/Users/Jonathan/.rvm/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin
-export LD_LIBRARY_PATH=usr/local/lib
+export PATH=/usr/local/bin:/usr/local/opt/ccache/libexec:/Users/Jonathan/Library/Haskell/bin:/Users/Jonathan/.cabal/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin
+export LD_LIBRARY_PATH=/usr/local/lib
+export DYLD_LIBRARY_PATH=/usr/local/lib
 export GIT_EDITOR=/usr/local/bin/vim
 export EDITOR=/usr/local/bin/vim
 export VISUAL=/usr/local/bin/vim
