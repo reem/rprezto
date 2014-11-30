ef5ae29e4beb775bdd1d5377df0fb3978299f810
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-30 02:46:05 -0500

    (feat) Support for new rust-bare.

--- runcoms/zshmodules/rust.zsh
+++ runcoms/zshmodules/rust.zsh
@@ -1,10 +1,26 @@
 # Rust aliases
 rust-create () {
-    git clone https://github.com/reem/rust-bare "rust-$1" && scd "rust-$1"
-    rm -rf .git README.md && sed -i '' "s/project-name/$1/g" Cargo.toml
-    git init
+    local name=$1
+    local description=${2:-""}
+
+    # Get and clear the repo.
+    quiet clone rust-bare "rust-$name"
+    rimraf .git README.md
+
+    # Insert name and description.
+    replace "project-name" "$name" Cargo.toml README.in.md
+    replace "project-description" "$description" Cargo.toml README.in.md
+
+    # Move template files to final locations.
+    mv README.in.md README.md
+
+    # Go!
+    quiet git init
+    ls
 }
 
 # Must be run as sudo
-rustup () { curlish https://static.rust-lang.org/rustup.sh bash }
+rustup () {
+    curlish https://static.rust-lang.org/rustup.sh bash
+}
 
