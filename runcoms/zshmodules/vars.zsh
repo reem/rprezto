8591bf2fa6b98202825f6ef451bbd466f68e2840
Jonathan Reem <jonathan.reem@gmail.com>
2015-06-03 15:55:52 -0700

    (feat) Add Go env vars and set RUST_BACKTRACE by default.

--- runcoms/zshmodules/vars.zsh
+++ runcoms/zshmodules/vars.zsh
@@ -1,11 +1,12 @@
 # Env Vars
-export PATH=/usr/local/bin:/usr/local/opt/ccache/libexec:/Users/Jonathan/Library/Haskell/bin:/Users/Jonathan/.cabal/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin
+export GOPATH=/Users/reem/code/go
+export PATH=/usr/local/opt/ccache/libexec:/usr/local/bin:/Users/reem/Library/Haskell/bin:/Users/reem/.cabal/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin:$GOPATH/bin
 export LD_LIBRARY_PATH=/usr/local/lib
-export DYLD_LIBRARY_PATH=/usr/local/lib
 export GIT_EDITOR=/usr/local/bin/vim
 export EDITOR=/usr/local/bin/vim
 export VISUAL=/usr/local/bin/vim
 export NODE_PATH=/usr/local/lib/node_modules
 export NODE_ENV="development"
+export RUST_BACKTRACE=1
 export XML_CATALOG_FILES="/usr/local/etc/xml/catalog"
 
