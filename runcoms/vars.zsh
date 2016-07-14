bc3ba5dc682ac82298a97da4f580c1c26ae3b681
Jonathan Reem <jonathan.reem@gmail.com>
2016-07-14 15:02:03 -0700

    Shorten $PATH definition.

--- runcoms/vars.zsh
+++ runcoms/vars.zsh
@@ -1,6 +1,6 @@
 # Env Vars
 export GOPATH=/Users/reem/code/go
-export PATH=/usr/local/opt/ccache/libexec:/usr/local/bin:/usr/local/sbin:/Users/reem/Library/Haskell/bin:/Users/reem/.cabal/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin:$GOPATH/bin
+export PATH=/usr/local/opt/ccache/libexec:/usr/local/bin:/usr/local/sbin:$HOME/.cargo/bin:$HOME/Library/Haskell/bin:$HOME/.cabal/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin:$GOPATH/bin
 export LD_LIBRARY_PATH=/usr/local/lib
 export GIT_EDITOR=/usr/local/bin/vim
 export EDITOR=/usr/local/bin/vim
