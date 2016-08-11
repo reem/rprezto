b762bb7a3ee3a86e2f00af72efe24ecc7c0f4059
Jonathan Reem <jonathan.reem@gmail.com>
2016-08-11 00:53:44 -0700

    New tdeploy aliases.

--- runcoms/zshmodules/tdeploy.zsh
+++ runcoms/zshmodules/tdeploy.zsh
@@ -17,9 +17,22 @@ alias tstf="tdeploy store fork --thawed"
 alias tstd="tdeploy store destroy"
 alias tstls="tdeploy store list"
 
-alias tssh="tact-ssh-to-main -d "
 alias tup="tact-spinup-cluster"
 alias tdown="tact-teardown-cluster"
+
+tssh () {
+   local cluster=$1
+   shift
+
+   if [[ -z $1 ]] then
+     ssh $cluster.test-cluster.com
+   else
+     local service=$1
+     shift
+
+     ssh -t $cluster.test-cluster.com vzentr $service
+   fi
+}
 
 tsre () {
    tso $*
@@ -41,4 +54,38 @@ tsup () {
 
    tsap -ve $target $*
    tso -ve $original $*
+}
+
+tsync () {
+  local domain=$1
+  shift
+  local service=$1
+  shift
+  local src=$1
+  shift
+  local dst=$1
+  shift
+
+  local ctid=$(tctid $domain $service)
+
+  KEY=~/.ssh/terminal-dev-default
+
+  rsync -e "ssh -i $KEY" --itemize-changes -az $@ $src root@$domain.test-cluster.com:/vz/root/$ctid/$dst
+}
+
+tctid () {
+  local domain=$1
+  local service=$2
+
+  KEY=~/.ssh/terminal-dev-default
+
+  CTIDS=$(ssh -i $KEY root@$domain.test-cluster.com  vzlist -Hao ctid -N 'cloudlabsreserved-'$service'-*' )
+  NWORDS=$(echo $CTIDS | wc -w | tr -d ' ')
+  if [ "$NWORDS" != "1" ]; then
+    echo "Expect exactly one container!"
+    exit 1
+  fi
+
+  CTID=$(echo $CTIDS | awk '{print $1;}')
+  echo $CTID
 }
